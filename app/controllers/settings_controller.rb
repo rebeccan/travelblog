788b261e3d99ced367252ea1fcf1f22f3f43c776
Rebecca N <rebecca@neigert-muenchen.de>
2014-10-05 13:39:50 +0200

    http_authentication_form against rails convention (object identification not working yet)

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,4 +1,17 @@
 class SettingsController < ApplicationController
   before_filter :authenticate_user!
   
+  def index
+    @http_authentication = Setting.find(1)
+  end
+  
+  def edit
+    @http_authentication = Setting.find(1)
+    if @http_authentication.save
+        redirect_to settings_path, alert: "Settings saved successfully."
+    else
+        redirect_to settings_path, alert: "Error."
+    end
+  end
+  
 end
